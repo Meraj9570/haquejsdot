fbc2accbcc16810e95b870b205ca45df9cfa109a
Meraj <haquedot@gmail.com>
2024-06-26 12:36:40 +0530

    varibles and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 11224
+let accountEmail = "haquedot@gmail.com"
+var accountPassword = "123124"
+accountCity = "SPJ"
+
+let accountState
+
+accountEmail = "meraj@gmail.com"
+accountPassword = "2352323"
+accountCity = "BGS"
+
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
